209e3aba914da23b0424d316a7f6501df7c2c059
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-01-13 00:32:32 +0100

    Impl: minor improvements for logging and saving

--- DeepRL/source.py
+++ DeepRL/source.py
@@ -11,27 +11,28 @@ import tensorflow as tf
 from tensorflow.keras import models, layers
 
 env = gym.make('BreakoutDeterministic-v4')
+now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
+checkpoint_path = os.path.join(
+    "models",
+    now,
+    "-{epoch:04d}.ckpt"
+)
 
-# frame = env.reset()
-# env.render()
+# checkpoint_dir = os.path.dirname(checkpoint_path)
 
+# # Create a callback that saves the model's weights
+# cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+#                                                  save_weights_only=True,
+#                                                  verbose=1)
 
-def build_function():
-    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-
-    def update_dir(idx):
-        log_dir = os.path.join(
-            "logs",
-            "fit",
-            now,
-            # "episode" + str(idx)
-        )
-        return log_dir
-
-    return update_dir
-
-time_func = build_function()
+log_dir = os.path.join(
+    "logs",
+    now,
+)
+tensorflow_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+file_writer_rewards = tf.summary.create_file_writer(log_dir + "/metrics")
+# file_writer.set_as_default()
 
 N = 1000
 D = list()
@@ -40,7 +41,7 @@ discount_rate = 0.8
 action_space = env.action_space.n
 input_shape = list(np.array(env.observation_space.shape) // 2)[:2] + [4]
 n_episode = 1000
-batch_size = 100
+batch_size = 500
 q_mask_shape = (batch_size, action_space)
 
 
@@ -70,12 +71,15 @@ mask = layers.Input(action_space, dtype=tf.float32)
 x = layers.Conv2D(32, (3, 3), activation="elu")(input)
 x = layers.MaxPooling2D((2, 2))(x)
 x = layers.BatchNormalization()(x)
+x = layers.Dropout(.4)(x)
 x = layers.Conv2D(32, (3, 3), activation="elu")(x)
 x = layers.MaxPooling2D((2, 2))(x)
 x = layers.BatchNormalization()(x)
+x = layers.Dropout(.4)(x)
 x = layers.Conv2D(32, (3, 3), activation="elu")(x)
 x = layers.MaxPooling2D((2, 2))(x)
 x = layers.BatchNormalization()(x)
+x = layers.Dropout(.4)(x)
 x = layers.Flatten()(x)
 x = layers.Dense(64, activation="relu")(x)
 x = layers.Dense(action_space)(x)
@@ -131,9 +135,11 @@ for episode in range(n_episode):
 
     next_state = initial_state.copy()
 
-    tensorflow_callback = tf.keras.callbacks.TensorBoard(log_dir=time_func(episode), histogram_freq=1)
+    episode_rewards = []
+    frame_cnt = 0
 
     while not is_done:
+        frame_cnt += 1
         if random.choices((True, False), (exploration_rate, 1 - exploration_rate))[0]:
             action = env.action_space.sample()
         else:
@@ -143,13 +149,17 @@ for episode in range(n_episode):
             action = np.argmax(q_values)
 
         next_observation, reward, is_done, _ = env.step(action)
-
+        episode_rewards.append(reward)
         # print(f"Current result: {reward}, {is_done}, {_}")
         # print("RENDER!")
         next_state.append(preprocess(next_observation))
         D.append((initial_state.copy(), reward, action, next_state.copy()))
         env.render()
-    # time.sleep(1)
+
+    with file_writer_rewards.as_default():
+        # tf.summary.scalar('episode_rewards_per_frame', np.sum(episode_rewards)/frame_cnt, step=episode)
+        tf.summary.scalar('episode_rewards', np.sum(episode_rewards), step=episode)
+        # tf.summary.scalar('episode_no_of_frames', frame_cnt, step=episode)
 
     experience_batch = random.sample(D, k=batch_size)
 
@@ -170,6 +180,11 @@ for episode in range(n_episode):
 
     next_q = set_of_batch_rewards + (discount_rate * tf.reduce_max(next_q_values, axis=1))
     history = model.fit([set_of_batch_initial_states, set_of_batch_actions], next_q, verbose=1, callbacks=[tensorflow_callback])
+    
+    if episode+1 % 100 == 0:
+        model_target_dir = checkpoint_path.format(epoch=episode)
+        model.save_weights(model_target_dir)
+        print(f"Model was saved under {model_target_dir}")
 
 # TODO: [x] Simplify the loss function
 # TODO: [x] Apply the reward
